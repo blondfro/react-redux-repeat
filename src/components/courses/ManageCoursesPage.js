6e742ba2627398899294afd96900d096972a70e9
James Morris <blondfro@gmail.com>
2020-03-22 22:00:41 -0500

    created an initial state file and use it in the 2 reducers.

---
+++ src/components/courses/ManageCoursesPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ManageCoursesPage = () => {
+  return <div>Stuff here.</div>;
+};
+
+export default ManageCoursesPage;
--- src/redux/reducers/authorReducer.js
+++ src/redux/reducers/authorReducer.js
@@ -1,6 +1,7 @@
 import * as actions from "../actions/actionTypes";
+import initialState from "./initialState";
 
-export default function authorReducer(state = [], action) {
+export default function authorReducer(state = initialState.authors, action) {
   switch (action.type) {
     case actions.CREATE_AUTHOR:
       return [...state, { ...action.author }];
